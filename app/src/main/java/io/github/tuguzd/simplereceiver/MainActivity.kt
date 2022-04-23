2fff1239ff2fff7549508fc0aab3b299028f8e27
Damir Tugushev <0damir.1tugushev@gmail.com>
2022-04-23 04:55:27 +0300

    Initial commit

---
+++ app/src/main/java/io/github/tuguzd/simplereceiver/MainActivity.kt
@@ -0,0 +1,28 @@
+package io.github.tuguzd.simplereceiver
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import io.github.tuguzd.simplereceiver.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button.setOnClickListener {
+            val text = binding.editText.text.toString()
+            val sendIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, text)
+                type = "text/plain"
+            }
+            val shareIntent = Intent.createChooser(sendIntent, null)
+            startActivity(shareIntent)
+        }
+    }
+}
